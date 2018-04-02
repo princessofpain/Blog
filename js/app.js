cc3897c881be1e48bedef24319b76ccaeb119acc
princessOfPain <magiclanzelot@gmx.net>
2018-04-02 13:23:00 +0200

    contact form functionality added + reset

--- js/app.js
+++ js/app.js
@@ -6,16 +6,18 @@ $(function() {
     emails: [],
     contacts:[],
 
-    contactData: function(name, email) {
+    contactData: function(name, email, occasion, message) {
       this.name = name;
       this.email = email;
+      this.occasion = occasion;
+      this.message = message;
     },
 
-    setContactData: function(name, email, boolean){
-      model.contacts.push(new model.contactData(name, email));
+    setContactData: function(name, email, checkbox, occasion, message){
+      model.contacts.push(new model.contactData(name, email, occasion, message));
 
       // if the checkbox was checked push email in the array for the newsletter
-      if(boolean === true) {
+      if(checkbox.checked === true) {
         model.checkDoubleMail(email);
       }
     },
@@ -29,7 +31,6 @@ $(function() {
         model.emails.push(email);
       }
     }
-
   };
 
   // build a bridge betweem model and view
@@ -40,34 +41,47 @@ $(function() {
 
     handleEmail: function(email) {
       model.setEmail(email);
+    },
+
+    handleContactData: function(name, email, checkbox, occasion, message) {
+      model.setContactData(name, email, checkbox, occasion, message);
     }
   };
 
   // here is everything related to the ui
-  var view = { //eslint-disable-line
+  var view = { 
 
-    init: function(){
-      $('#subscribe-button').submit(view.submitEmail);
-      $('#search-button').sumbit(view.search);
-      $('#contact-button').sumbit(view.contactData);
-      $('#comment-button').sumbit(view.comment);
+    init: function() {
+      $('#subscribe-button').click(view.submitEmail);
+      $('#search-button').click(view.submitSearch);
+      $('#contact-button').click(view.submitContactData);
+      $('#comment-button').click(view.submitComment);
     },
 
     submitEmail: function() {
-      const subscribedEmail = $('#email-input');
-      control.handleEmail(subscribedEmail);
-      subscribedEmail.text('');
+      control.handleEmail($('#email-input').val());
+      $('#email-input').val('');
     },
 
-    search: function() {
-
-    },
-
-    contact: function() {
+    submitSearch: function() {
 
     },
 
-    comment: function() {
+    submitContactData: function() {
+      const occasion = $('#select-occasion option:selected');
+
+      control.handleContactData($('#contact-name').val(), $('#contact-email').val(), 
+        $('#checkbox-newsletter'), occasion, $('#contact-text'));
+
+      $('#contact-name').val('');
+      $('#contact-email').val('');
+      $('#checkbox-newsletter').checked;
+      $('#select-occasion option').removeAttr('selected');
+      $('#select-occasion option[value="default"]').attr('selected', true);
+      $('#contact-text').val('');
+    },
+
+    submitComment: function() {
 
     }
   };
